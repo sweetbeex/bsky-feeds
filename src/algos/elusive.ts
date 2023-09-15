311cef3ed1ae12580474d46d38e9dee5b5088a72
sweetbeex <138159061+sweetbeex@users.noreply.github.com>
2023-09-14 21:34:32 -0700

    algo replace

--- src/algos/elusive.ts
+++ src/algos/elusive.ts
@@ -1,83 +1,82 @@
-import { QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import { AppContext } from '../config'
-import { AlgoManager } from '../addn/algoManager'
-import dotenv from 'dotenv'
-import { Post } from '../db/schema'
-import dbClient from '../db/dbClient'
-import getUserFollows from '../addn/getUserFollows'
+import { QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton';
+import { AppContext } from '../config';
+import { AlgoManager } from '../addn/algoManager';
+import dotenv from 'dotenv';
+import { Post } from '../db/schema';
+import dbClient from '../db/dbClient';
+import getUserFollows from '../addn/getUserFollows';
 
-dotenv.config()
+dotenv.config();
 
 // max 15 chars
-export const shortname = 'baddies'
+export const shortname = 'baddies';
 
 export const handler = async (ctx: AppContext, params: QueryParams) => {
   const builder = await dbClient.getLatestPostsForTag(
     shortname,
     params.limit,
     params.cursor,
-  )
+  );
 
   const feed = builder.map((row) => ({
     post: row.uri,
-  }))
+  }));
 
-  let cursor: string | undefined
-  const last = builder.at(-1)
+  let cursor: string | undefined;
+  const last = builder.at(-1);
   if (last) {
-    cursor = `${new Date(last.indexedAt).getTime()}::${last.cid}`
+    cursor = `${new Date(last.indexedAt).getTime()}::${last.cid}`;
   }
 
   return {
     cursor,
     feed,
-  }
-}
+  };
+};
 
 export class manager extends AlgoManager {
-  public name: string = shortname
-  public did = 'did:plc:db645kt5coo7teuoxdjhq34x'
-  public follows: string[] = []
+  public name: string = shortname;
+  public did = 'did:plc:db645kt5coo7teuoxdjhq34x';
+  public follows: string[] = [];
+
+  // Define the regular expression
+  public re: RegExp = /!nopromote/; // Replace with your actual regex pattern
 
   public async periodicTask() {
-    this.follows = await getUserFollows(this.did, this.agent)
+    this.follows = await getUserFollows(this.did, this.agent);
     await this.db.removeTagFromOldPosts(
       this.name,
-      new Date().getTime() - 3 * 24 * 60 * 60 * 1000, //3 days
-    )
+      new Date().getTime() - 3 * 24 * 60 * 60 * 1000, // 3 days
+    );
   }
 
-  public async filter_post(post: Post): Promise<Boolean> {
-    if (post.replyRoot !== null) return false
-    // getUserFollows is memoised, so this should be fine
-    this.follows = await getUserFollows(this.did, this.agent)
+  public async filter_post(post: Post): Promise<boolean> {
+    if (post.replyRoot !== null) return false;
+    // getUserFollows is memoized, so this should be fine
+    this.follows = await getUserFollows(this.did, this.agent);
 
     if (this.agent === null) {
-      await this.start()
+      await this.start();
     }
-    if (this.agent === null) return false
+    if (this.agent === null) return false;
 
-    let return_value: Boolean | undefined = undefined
-
-    let match = false
-
-    let matchString = ''
+    let return_value: boolean | undefined = undefined;
+    let match = false;
+    let matchString = '';
 
     if (post.embed?.images) {
-      const imagesArr = post.embed.images
+      const imagesArr = post.embed.images;
       imagesArr.forEach((image) => {
-        matchString = `${matchString} ${image.alt}`.replace('\n', ' ')
-      })
+        matchString = `${matchString} ${image.alt}`.replace('\n', ' ');
+      });
     }
 
-    matchString = `${post.text} ${matchString}`.replace('\n', ' ')
+    matchString = `${post.text} ${matchString}`.replace('\n', ' ');
 
     if (matchString.match(this.re) !== null) {
-      match = true
+      match = true;
     }
 
-    return match    
-
-    return this.follows.includes(post.author)
+    return match;
   }
 }
